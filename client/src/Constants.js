617cda8c019167a505ac063a3336fcd87180acb4
Monika Osborn <127253595+protonWitch@users.noreply.github.com>
2025-10-15 12:53:38 -0600

    fix: Update server URL in Constants.js and refresh binary cache files

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,3 +1,4 @@
 // export const serverUrl = "unholyfunk.com";
-export const serverUrl = "/server";
+export const serverUrl =
+  "https://book-club-app-f8pn5.ondigitalocean.app/server";
 // export const serverUrl = "localhost:5000";
