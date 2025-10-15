da48c275a01c3fe0a3885f3093fd13f134e84283
Monika Osborn <127253595+protonWitch@users.noreply.github.com>
2025-10-15 12:57:42 -0600

    fix: Update server URL format in Constants.js and enhance .gitignore for backend files

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,4 +1,3 @@
 // export const serverUrl = "unholyfunk.com";
-export const serverUrl =
-  "https://book-club-app-f8pn5.ondigitalocean.app/server";
+export const serverUrl = "book-club-app-f8pn5.ondigitalocean.app/server";
 // export const serverUrl = "localhost:5000";
